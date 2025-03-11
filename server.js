5000137504877cb8653c6886e5850cdbbe5084c4
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-03-11 16:32:58 +0530

    fixed

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("test");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+  }
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", async (req, res)=>{
+    res.json("working");
+})
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("App is listening on PORT :-", process.env.PORT);
+})
